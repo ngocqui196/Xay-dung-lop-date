e9717ab22387d3505f34cb999972d88f5451a5e4
ngocqui196 <58946367+ngocqui196@users.noreply.github.com>
2020-07-14 15:05:25 +0700

    Sửa lại

--- date.js
+++ date.js
@@ -1,6 +1,6 @@
 // Tham số : day, month, year.
-// 3-27 Khai báo lớp myDate. trong javascript không có từ khóa cụ thể để khaibaso lóp nên phải dùng từ khóa funtion để khai báo.
-let myDate = function (day, month, year) {
+// 3-27 Khai báo lớp myDate. trong javascript không có từ khóa cụ thể để khai báo lóp nên phải dùng từ khóa funtion để khai báo.
+let MyDate = function (day, month, year) {
     // Thuộc tính là tính chất
     this.day = day; // Gán tham số màu trắng "day"  vào thuộc tính màu tím "day"
     this.month = month;
@@ -25,17 +25,17 @@ let myDate = function (day, month, year) {
         this.year = year;
     }
 };
-let date = new myDate(90 ,60, 90); //Tạo ra đối tượng date có các tham số
+let date = new MyDate(15 ,3, 1990); //Tạo ra đối tượng date thuộc lớp MyDate và truyền  vào các giá trị tương ứng với từng thuộc tính
 let day = date.getDay();  // Lấy tham số thuộc tính "day" của đối tượng "date"
 let month = date.getMonth(); //lấy tham số của thuộc tính "month" của đối tượng "date"
-let year = date.getYear();
+let year = date.getYear();    // Lấy tham số của thuộc tính "year" của đối tượng "date"
 alert(day + '/' + month + '/' + year);
 
 
-date.setDay(95); //Sửa lại thuộc tính "day" của đối tượng của 'date'
-date.setMonth(52);
-date.setYear(96);
-day = date.getDay(); // Khai bá
+date.setDay(10); //Sửa lại thuộc tính "day" của đối tượng của 'date'
+date.setMonth(10);
+date.setYear(2019);
+day = date.getDay(); // Nhận giá trị của thuộc tính "this.day" vừa sửa và gán cho nó vào biến "day"
 month = date.getMonth();
 year = date.getYear();
 alert(day + "/" + month + "/" + year);
